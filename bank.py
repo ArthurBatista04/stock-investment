3cb26927723db453816810a21f672addbb5287a5
arthurbatista04 <arthurbatista04@outlook.com>
2020-10-01 11:46:11 -0300

    clean code + profits per month

--- bank.py
+++ bank.py
@@ -1,19 +1,20 @@
+import math
 class Bank(object):
-    def __init__(self, safe_box):
-        self.number_of_shares = 0
-        self.safe_box = safe_box
-        self.initial_value = safe_box
+	def __init__(self, safe_box):
+		self.number_of_shares = 0
+		self.safe_box = safe_box
+		self.initial_value = safe_box
 
-    def purchase(self, porcentage, value_per_share):
-        if(self.safe_box > 0 and self.safe_box >= value_per_share):
-            value_to_buy = self.safe_box * porcentage
-            while(value_to_buy >= value_per_share):
-                self.safe_box -= value_per_share
-                self.number_of_shares += 1
-                value_to_buy -= value_per_share
+	def purchase(self, porcentage, value_per_share):
+		if(self.safe_box > 0 and self.safe_box >= value_per_share):
+			value_to_buy = self.safe_box * porcentage
+			while(value_to_buy >= value_per_share):
+				self.safe_box -= value_per_share
+				self.number_of_shares += 1
+				value_to_buy -= value_per_share
 
-    def sell(self, porcentage, value_per_share):
-        if(self.number_of_shares > 1):
-            number_of_shares_to_sell = abs(self.number_of_shares * porcentage)
-            self.number_of_shares -= number_of_shares_to_sell
-            self.safe_box += number_of_shares_to_sell * value_per_share
+	def sell(self, porcentage, value_per_share):
+		if(self.number_of_shares >= 1):
+			number_of_shares_to_sell = math.floor(self.number_of_shares * porcentage)
+			self.number_of_shares -= number_of_shares_to_sell
+			self.safe_box += number_of_shares_to_sell * value_per_share
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,107 +3,98 @@ import math
 
 
 class NODE(object):
-    def __init__(self, gene):
-        self.fitness = 0
-        self.gene = gene
+	def __init__(self, gene):
+		self.fitness = 0
+		self.gene = gene
 
-    def calculate_fitness(self, indicators):
-        for i in range(len(indicators)):
-            self.fitness += self.gene[i] * indicators[i]
+	def calculate_fitness(self, indicators):
+		for i in range(len(indicators)):
+			self.fitness += self.gene[i] * indicators[i]
 
 
 def selection(population):
-    individuos = []
-    random_positions = random.sample([i for i in range(len(population))], 32)
-    for i in random_positions:
-        individuos.append(population[i])
-    parents = sorted(individuos, key=lambda agent: agent.fitness, reverse=True)
-    return [parents[0], parents[1]]
+	individuos = []
+	random_positions = random.sample([i for i in range(len(population))], 32)
+	for i in random_positions:
+		individuos.append(population[i])
+	parents = sorted(individuos, key=lambda agent: agent.fitness)
+	return [parents[0], parents[1]]
 
 
 def crossing_over(father, mother):
-    required_gene_number = len(father.gene)
-    child_gene = [-1 for _ in range(required_gene_number)]
+	required_gene_number = len(father.gene)
+	total_sum = sum(father.gene) + sum(mother.gene)
 
-    for i in range(required_gene_number):
-        if(father.gene[i] == mother.gene[i]):
-            child_gene[i] = father.gene[i]
-
-    while(-1 in child_gene):
-        random_position = random.randrange(0, required_gene_number)
-        if(child_gene[random_position] == -1):
-            choice_father_gene = random.randrange(0, 2)
-            if(choice_father_gene):
-                child_gene[random_position] = father.gene[random_position]
-            else:
-                child_gene[random_position] = mother.gene[random_position]
-    return NODE(child_gene)
+	child_gene = [(mother.gene[i] + father.gene[i]) /
+				  total_sum for i in range(required_gene_number)]
+	return NODE(child_gene)
 
 
 def initial_population(population_size, gene_size):
-    population = []
-    count = 0
-    while(count < population_size):
-        random_values = [random.random() for _ in range(gene_size)]
-        sum_values = sum(random_values)
-        gene = [i/sum_values for i in random_values]
-        if (sum(gene) == 1.0):
-            population.append(NODE(gene))
-            count += 1
-    return population
+	population = []
+	count = 0
+	while(count < population_size):
+		random_values = [random.random() for _ in range(gene_size)]
+		sum_values = sum(random_values)
+		gene = [i/sum_values for i in random_values]
+		if (sum(gene) == 1.0):
+			population.append(NODE(gene))
+			count += 1
+	return population
 
 
 def fitness(population, indicators):
-    for individual in population:
-        individual.calculate_fitness(indicators)
+	for individual in population:
+		individual.calculate_fitness(indicators)
 
 
 def mutation(child):
-    is_mutating = random.randrange(0, 2)
-    if(is_mutating):
-        gene_length = len(child.gene)
-        rand_1, rand_2 = random.sample([i for i in range(gene_length)], 2)
-        child.gene[rand_1], child.gene[rand_2] = child.gene[rand_2], child.gene[rand_1]
-    return child
+	is_mutating = random.randrange(0, 2)
+	if(is_mutating):
+		gene_length = len(child.gene)
+		rand_1, rand_2 = random.sample([i for i in range(gene_length)], 2)
+		child.gene[rand_1], child.gene[rand_2] = child.gene[rand_2], child.gene[rand_1]
+	return child
 
 
-def get_weakest_individual_index(population):
-    weakest = math.inf
-    index_weakest = 0
-    for (index, individual) in enumerate(population):
-        if (individual.fitness < weakest):
-            weakest = individual.fitness
-            index_weakest = index
-    return index_weakest
+def get_lowest_fitness_index(population):
+	lowest = math.inf
+	index_lowest = 0
+	for (index, individual) in enumerate(population):
+		if (individual.fitness < lowest):
+			lowest = individual.fitness
+			index_lowest = index
+	return index_lowest
 
 
-def get_strongest_individual_index(population):
-    strongest = -math.inf
-    index_strongest = 0
-    for (index, individual) in enumerate(population):
-        if (individual.fitness > strongest):
-            strongest = individual.fitness
-            index_strongest = index
-    return index_strongest
+def get_highest_fitness_index(population):
+	highest = -math.inf
+	index_highest = 0
+	for (index, individual) in enumerate(population):
+		if (individual.fitness > highest):
+			highest = individual.fitness
+			index_highest = index
+	return index_highest
 
 
-def include_in_population(individuo, population):
-    weakest_index = get_weakest_individual_index(population)
-    weakest = population[weakest_index]
-    return individuo.fitness < weakest.fitness
+def include_child_in_population(child, population):
+	lowest_index = get_lowest_fitness_index(population)
+	lowest = population[lowest_index]
+	return child.fitness < lowest.fitness
 
 
 def genetic_algorithm(population_size, indicators):
-    population = initial_population(population_size, len(indicators))
-    fitness(population, indicators)
+	population = initial_population(population_size, len(indicators))
+	fitness(population, indicators)
 
-    for _ in range(500):
-        father, mother = selection(population)
-        child = crossing_over(father, mother)
-        mutated_child = mutation(child)
-        mutated_child.calculate_fitness(indicators)
+	for _ in range(500):
+		father, mother = selection(population)
+		child = crossing_over(father, mother)
+		mutated_child = mutation(child)
+		mutated_child.calculate_fitness(indicators)
 
-        if(include_in_population(mutated_child, population)):
-            population.pop(get_weakest_individual_index(population))
-            population.append(mutated_child)
-    return population[get_weakest_individual_index(population)], population[get_strongest_individual_index(population)]
+		if(include_child_in_population(mutated_child, population)):
+			population.pop(get_lowest_fitness_index(population))
+			population.append(mutated_child)
+
+	return population[get_lowest_fitness_index(population)]
